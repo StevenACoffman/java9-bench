d6f4b24e3e2f94d21a8d7944e8a968e91423320e
Steve Coffman <steve.coffman@ithaka.org>
2017-11-20 21:10:22 -0500

    master: Initial add

---
+++ src/mjg123.module/mjg123/module/Main.java
@@ -0,0 +1,8 @@
+package mjg123.module;
+
+public class Main {
+  public static void main(String... args){
+    System.out.println("Hello from mjg123.module");
+  }
+}
+
---
+++ src/mjg123.module/module-info.java
@@ -0,0 +1,1 @@
+module mjg123.module {}
